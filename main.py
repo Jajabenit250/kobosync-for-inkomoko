6fa2d61735e4fb33d681b140d293cbb347596041
Havugimana Benit <jajabenit@gmail.com>
2024-09-02 04:42:44 +0200

    updates to improve information security

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import uvicorn
+import os
 
 if __name__ == '__main__':
     uvicorn.run(
         'app:app',
         host="0.0.0.0",
-        port=8000,
+        port=os.getenv("PORT", "8000"),
         reload=True
     )
